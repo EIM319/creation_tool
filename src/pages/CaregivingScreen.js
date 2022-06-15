b4663d557c6575ba0d6d342ef5a419857217c4e9
snehakumar27 <snehakumar27707@gmail.com>
2022-06-15 20:51:43 +0700

    caregiving/homemonitoring base

---
+++ src/pages/CaregivingScreen.js
@@ -0,0 +1,7 @@
+export default function CaregivingScreen() {
+	return (
+    <main style={{ padding: "1rem 0" }}>
+    <h2>Caregiving</h2>
+  </main>
+    )
+}
---
+++ src/pages/HomeMonitoringScreen.js
@@ -0,0 +1,7 @@
+export default function HomeMonitoringScreen() {
+	return (
+        <main style={{ padding: "1rem 0" }}>
+        <h2>HomeMonitoring</h2>
+      </main>
+    )
+}
