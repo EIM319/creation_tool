2a4929db58b53d19d82b1a0ddc0279c8cf413e19
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-06-21 12:40:12 +0800

    link caregiving

--- src/pages/CaregivingScreen.js
+++ src/pages/CaregivingScreen.js
@@ -1,7 +1,65 @@
-export default function CaregivingScreen() {
+import { useEffect, useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { doc, getDoc } from "firebase/firestore/lite";
+import EditArticleComponent from "../components/articles/EditArticleComponent";
+
+export default function CaregivingScreen({ database, userName }) {
+	const [caregiving, setCaregiving] = useState();
+	const [selectedCaregiving, setSelectedCaregiving] = useState();
+
+	useEffect(() => {
+		getCaregiving(database, userName, setCaregiving);
+	}, []);
+
+	if (caregiving === undefined) return <></>;
+	const caregivingList = [];
+	for (let i = 0; i < caregiving.length; i++) {
+		const article = caregiving[i];
+		caregivingList.push(
+			<div
+				className="toggle"
+				style={{
+					width: "100%",
+					paddingLeft: 30,
+					paddingRight: 30,
+				}}
+				onClick={() => {
+					setSelectedCaregiving(article);
+				}}
+				key={i}
+			>
+				<p>{article.name}</p>
+			</div>
+		);
+	}
+
 	return (
-    <main style={{ padding: "1rem 0" }}>
-    <h2>Caregiving</h2>
-  </main>
-    )
+		<div>
+			<Row style={{ width: "100%" }}>
+				<Col xs={3} style={{ padding: 30 }}>
+					{caregivingList}
+					<Button variant="secondary" style={{ width: "100%" }}>
+						Add
+					</Button>
+				</Col>
+				<Col xs={9} style={{ padding: 30 }}>
+					<EditArticleComponent
+						articles={caregiving}
+						setArticles={setCaregiving}
+						article={selectedCaregiving}
+						database={database}
+						userName={userName}
+						type="caregiving"
+					/>
+				</Col>
+			</Row>
+		</div>
+	);
+}
+
+async function getCaregiving(database, userName, setCaregiving) {
+	const ref = doc(database, "users", userName);
+	const result = await getDoc(ref);
+	const data = result.data();
+	setCaregiving(data.caregiving);
 }
