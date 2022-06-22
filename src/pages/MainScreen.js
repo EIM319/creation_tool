bc9f5551edceeeeec53d81ed5863fe9fc2f01a9c
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-06-22 09:01:55 +0800

    Update main

--- src/pages/MainScreen.js
+++ src/pages/MainScreen.js
@@ -1,4 +1,5 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import AdditionalNotesScreen from "./AdditionalNotes";
 import CaregivingScreen from "./CaregivingScreen";
 import HomeMonitoringScreen from "./HomeMonitoringScreen";
@@ -8,9 +9,19 @@ import MedicationScreen from "./MedicationScreen";
 const userName = "iCgfe1IHSfDNRC3hfgxF";
 
 export default function MainScreen({ database }) {
+	const [path, setPath] = useState("");
+	const location = useLocation();
+
+	useEffect(() => {
+		const currentPath = window.location.pathname.substring(1);
+		if (currentPath != path) {
+			setPath(currentPath);
+		}
+	}, [location]);
+
 	return (
 		<div className="mainPage">
-			<SideNavBar />
+			<SideNavBar path={path} />
 			<div className="content">
 				<Routes>
 					<Route
@@ -87,14 +98,26 @@ var screenNames = [
 	},
 ];
 
-function SideNavBar() {
+function SideNavBar({ path }) {
 	var toggles = [];
 	for (let i = 0; i < screenNames.length; i++) {
-		toggles.push(
-			<Link to={screenNames[i].path} className="sideNavText" key={i}>
-				{screenNames[i].name}
-			</Link>
-		);
+		if (screenNames[i].path === path) {
+			toggles.push(
+				<Link
+					to={screenNames[i].path}
+					className="sideNavText sideNavTextSelected"
+					key={i}
+				>
+					{screenNames[i].name}
+				</Link>
+			);
+		} else {
+			toggles.push(
+				<Link to={screenNames[i].path} className="sideNavText" key={i}>
+					{screenNames[i].name}
+				</Link>
+			);
+		}
 	}
 	return <div className="sideNav">{toggles}</div>;
 }
