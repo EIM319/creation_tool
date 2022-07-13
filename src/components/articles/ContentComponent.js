60c64bc4a5d87136213e70ebfd4095b4835ed58a
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-07-13 15:33:40 +0800

    Preview editing

--- src/components/articles/ContentComponent.js
+++ src/components/articles/ContentComponent.js
@@ -5,7 +5,9 @@ import {
 	FormControl,
 	Dropdown,
 	Button,
+	Image,
 } from "react-bootstrap";
+import YouTube from "./Youtube";
 
 export default function ContentComponent({
 	content,
@@ -23,71 +25,37 @@ export default function ContentComponent({
 	}, [content]);
 
 	return (
-		<InputGroup style={{ paddingBottom: 10 }}>
-			<DropdownButton title={getTypeString(type)} variant="light">
-				<Dropdown.Item
+		<>
+			<InputGroup style={{ paddingBottom: 10 }}>
+				<Content
+					type={type}
+					value={value}
+					setValue={setValue}
+					content={content}
+					index={index}
+				/>
+				<DropDown
+					type={type}
+					setType={setType}
+					content={content}
+					index={index}
+				/>
+				<Button
+					variant="danger"
 					onClick={() => {
-						setType("instruction");
-						content[index].type = "instruction";
+						const newArticle = new Object(article);
+						const newContent = [...content];
+						newContent.splice(index, 1);
+						newArticle.content = newContent;
+						setContent(newContent);
+						setArticle(newArticle);
 					}}
 				>
-					Text
-				</Dropdown.Item>
-				<Dropdown.Item
-					onClick={() => {
-						setType("section");
-						content[index].type = "section";
-					}}
-				>
-					Header
-				</Dropdown.Item>
-				<Dropdown.Item
-					onClick={() => {
-						setType("note");
-						content[index].type = "note";
-					}}
-				>
-					Note
-				</Dropdown.Item>
-				<Dropdown.Item
-					onClick={() => {
-						setType("image");
-						content[index].type = "image";
-					}}
-				>
-					Image
-				</Dropdown.Item>
-				<Dropdown.Item
-					onClick={() => {
-						setType("video");
-						content[index].type = "video";
-					}}
-				>
-					Video
-				</Dropdown.Item>
-			</DropdownButton>
-			<FormControl
-				as="textarea"
-				value={value}
-				onChange={(event) => {
-					setValue(event.target.value);
-					content[index].content = event.target.value;
-				}}
-			/>
-			<Button
-				variant="danger"
-				onClick={() => {
-					const newArticle = new Object(article);
-					const newContent = [...content];
-					newContent.splice(index, 1);
-					newArticle.content = newContent;
-					setContent(newContent);
-					setArticle(newArticle);
-				}}
-			>
-				X
-			</Button>
-		</InputGroup>
+					X
+				</Button>
+			</InputGroup>
+			<ExtraContent type={type} value={value} />
+		</>
 	);
 }
 
@@ -105,5 +73,142 @@ function getTypeString(type) {
 			return "Video";
 		default:
 			return "Invalid";
+	}
+}
+
+function DropDown({ setType, content, index, type }) {
+	return (
+		<DropdownButton title={getTypeString(type)} variant="light">
+			<Dropdown.Item
+				onClick={() => {
+					setType("instruction");
+					content[index].type = "instruction";
+				}}
+			>
+				Text
+			</Dropdown.Item>
+			<Dropdown.Item
+				onClick={() => {
+					setType("section");
+					content[index].type = "section";
+				}}
+			>
+				Header
+			</Dropdown.Item>
+			<Dropdown.Item
+				onClick={() => {
+					setType("note");
+					content[index].type = "note";
+				}}
+			>
+				Note
+			</Dropdown.Item>
+			<Dropdown.Item
+				onClick={() => {
+					setType("image");
+					content[index].type = "image";
+				}}
+			>
+				Image
+			</Dropdown.Item>
+			<Dropdown.Item
+				onClick={() => {
+					setType("video");
+					content[index].type = "video";
+				}}
+			>
+				Video
+			</Dropdown.Item>
+		</DropdownButton>
+	);
+}
+
+function Content({ type, value, setValue, content, index }) {
+	const list = [];
+	if (type === "video") {
+		list.push(
+			<InputGroup.Text>https://www.youtube.com/embed/</InputGroup.Text>
+		);
+	}
+	switch (type) {
+		case "instruction": {
+			list.push(
+				<FormControl
+					as="textarea"
+					value={value}
+					onChange={(event) => {
+						setValue(event.target.value);
+						content[index].content = event.target.value;
+					}}
+				/>
+			);
+			break;
+		}
+		case "note": {
+			list.push(
+				<FormControl
+					style={{ fontStyle: "italic" }}
+					as="textarea"
+					value={value}
+					onChange={(event) => {
+						setValue(event.target.value);
+						content[index].content = event.target.value;
+					}}
+				/>
+			);
+			break;
+		}
+		case "section": {
+			list.push(
+				<FormControl
+					style={{ fontSize: 20, fontWeight: 500 }}
+					value={value}
+					onChange={(event) => {
+						setValue(event.target.value);
+						content[index].content = event.target.value;
+					}}
+				/>
+			);
+			break;
+		}
+		default: {
+			list.push(
+				<FormControl
+					value={value}
+					onChange={(event) => {
+						setValue(event.target.value);
+						content[index].content = event.target.value;
+					}}
+				/>
+			);
+		}
+	}
+
+	return list;
+}
+
+function ExtraContent({ type, value }) {
+	switch (type) {
+		case "video": {
+			return (
+				<div style={{ width: 600 }}>
+					<YouTube id={value} />
+				</div>
+			);
+		}
+		case "image": {
+			return (
+				<Image
+					src={value}
+					style={{
+						width: 300,
+						aspectRatio: 1,
+						objectFit: "contain",
+					}}
+				/>
+			);
+		}
+		default:
+			return null;
 	}
 }
--- src/components/articles/Youtube.js
+++ src/components/articles/Youtube.js
@@ -1,9 +1,9 @@
-export default function YouTube({ url }) {
+export default function YouTube({ id }) {
 	return (
 		<iframe
 			width="90%"
 			style={{ aspectRatio: 1.778, marginBottom: 20 }}
-			src={url}
+			src={"https://www.youtube.com/embed/" + id}
 			frameBorder={0}
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 			allowFullScreen
