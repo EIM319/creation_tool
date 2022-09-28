dbc4dcc9eb2d628245050f7f091673134e49f849
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-09-28 08:53:31 +0800

    Add linking of additional notes

---
+++ src/components/notes/LinkItemPopup.js
@@ -0,0 +1,62 @@
+import { Modal, Button } from "react-bootstrap";
+
+export default function LinkItemPopup({
+	show,
+	setShow,
+	monitoring,
+	medication,
+	setAttachedType,
+	setAttachedItem,
+	additionalNotes,
+}) {
+	function MonitoringList() {
+		var itemList = [];
+		monitoring.forEach((article) => {
+			itemList.push(
+				<p
+					className="toggle"
+					onClick={() => {
+						setAttachedType("monitoring");
+						setAttachedItem(article.name);
+						additionalNotes.attachedType = "monitoring";
+						additionalNotes.attachedItem = article.name;
+						setShow(false);
+					}}
+				>
+					{article.name}
+				</p>
+			);
+		});
+		return itemList;
+	}
+
+	return (
+		<Modal
+			show={show}
+			onHide={() => {
+				setShow(false);
+			}}
+			centered
+		>
+			<Modal.Header>
+				<Modal.Title>Link Medication / Patient Education</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<MonitoringList />
+			</Modal.Body>
+			<Modal.Footer>
+				<Button
+					onClick={() => {
+						setAttachedType(null);
+						setAttachedItem(null);
+						additionalNotes.attachedType = null;
+						additionalNotes.attachedItem = null;
+						setShow(false);
+					}}
+				>
+					None
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
+}
